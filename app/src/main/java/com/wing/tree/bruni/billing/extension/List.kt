cd27aa2b36adfe9e372dc2fd94b22680c51aba84
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-12 22:21:04 +0900

    feat : Add new extension functions

--- app/src/main/java/com/wing/tree/bruni/billing/extension/List.kt
+++ app/src/main/java/com/wing/tree/bruni/billing/extension/List.kt
@@ -1,9 +1,14 @@
 package com.wing.tree.bruni.billing.extension
 
+import com.android.billingclient.api.Purchase
 import com.wing.tree.bruni.billing.model.Product
 
-operator fun List<Product>.get(id: String) = find { it.id == id }
+operator fun List<Product>.get(id: String): Product? = find { it.id == id }
+operator fun List<Product>.get(purchase: Purchase): Product? = find {
+    purchase.products.contains(it.id)
+}
 
 fun List<Product>.consumable() = filterIsINAPP().filter { it.consumable }
+fun List<Product>.contains(purchase: Purchase) = map { it.id }.containsAll(purchase.products)
 fun List<Product>.filterIsINAPP() = filterIsInstance<Product.INAPP>()
 fun List<Product>.filterIsSUBS() = filterIsInstance<Product.SUBS>()
